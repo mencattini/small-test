a494c82b1912c63eb981506f2a608400fe882cf2
Romain Mencattini <romain.mencattini@zaclys.net>
2019-01-29 16:26:55 +0100

    add repr function and change relation to 'many to many'

--- code/model.py
+++ code/model.py
@@ -1,5 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, Table
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    PrimaryKeyConstraint,
+    create_engine,
+    Table,
+)
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
@@ -9,6 +17,7 @@ association_table = Table(
     Base.metadata,
     Column("user_id", Integer, ForeignKey("users.user_id")),
     Column("skill_id", Integer, ForeignKey("skills.skill_id")),
+    PrimaryKeyConstraint("user_id", "skill_id"),
 )
 
 
@@ -21,9 +30,16 @@ class User(Base):
     fullname = Column(String)
     email = Column(String)
     address = Column(String)
-    mobile_phone = Column(Integer)
+    mobile_phone = Column(String)
 
-    skills = relationship("Skill", secondary=association_table)
+    skills = relationship("Skill", secondary=association_table, backref="users")
+
+    def __repr__(self):
+        return (
+            f"User(user_id={self.user_id}, firstname={self.firstname}, lastname={self.lastname}"
+            + f", fullname={self.fullname}, email={self.email}, address={self.address},"
+            + f" mobile_phone={self.mobile_phone}, skills={[ele.skill_id for ele in self.skills]})"
+        )
 
 
 class Skill(Base):
@@ -32,6 +48,9 @@ class Skill(Base):
     skill_id = Column(Integer, primary_key=True)
     name = Column(String)
     level = Column(String)
+
+    def __repr__(self):
+        return f"Skill(skill_id={self.skill_id}, name={self.name}, level={self.level}"
 
 
 engine = create_engine("sqlite:///../sqlite3.db")
