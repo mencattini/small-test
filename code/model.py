d231221b03219df4db65892c0248a9bf318013dd
Romain Mencattini <romain.mencattini@zaclys.net>
2019-01-29 14:50:41 +0100

    remove credential and try to add many to many relations

--- code/model.py
+++ code/model.py
@@ -1,38 +1,37 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, Table
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+association_table = Table(
+    "association",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.user_id")),
+    Column("skill_id", Integer, ForeignKey("skills.skill_id")),
+)
 
 
 class User(Base):
     __tablename__ = "users"
 
+    user_id = Column(Integer, primary_key=True)
     firstname = Column(String)
     lastname = Column(String)
-    fullname = Column(String, primary_key=True)
-    email = Column(String, primary_key=True)
+    fullname = Column(String)
+    email = Column(String)
     address = Column(String)
     mobile_phone = Column(Integer)
+
+    skills = relationship("Skill", secondary=association_table)
 
 
 class Skill(Base):
     __tablename__ = "skills"
 
-    id = Column(Integer, primary_key=True)
-    user_fullname = Column(String, ForeignKey("users.fullname"))
-    user_email = Column(String, ForeignKey("users.email"))
+    skill_id = Column(Integer, primary_key=True)
     name = Column(String)
     level = Column(String)
-
-
-class Credentials(Base):
-    # very very very very bad practices
-    __tablename__ = "credentials"
-
-    id = Column(Integer, primary_key=True)
-    user_fullname = Column(String, ForeignKey("users.fullname"))
-    user_email = Column(String, ForeignKey("users.email"))
-    password = Column(String)
 
 
 engine = create_engine("sqlite:///../sqlite3.db")
